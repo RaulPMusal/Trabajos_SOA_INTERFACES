71116de280ef28d5b37e3a0fdf7a2f8641e55b5b
MikelInesta <mikel_inesta@hotmail.com>
2023-12-27 21:38:19 +0100

    cambio metodo get a post, recibe el cuerpo de la respuesta y transforma el json en una lista de listas

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
+import json
 
 app = FastAPI()
 
-@app.get("/apilineal")
-async def lineal():
-    #obtener el json de la llamada post
+@app.post("/apilineal")
+async def lineal(request: Request):
+    # Se obtiene el json del cuerpo de la petición POST
+    data = await request.json() # Devuelve una lista de listas
 
-    #enviar el json a la funcion de la api lineal
-
-    #retornar el json de la respuesta
-    return {"mensaje": "apiLineal"};
-
-@app.get("/apinolineal")
-async def nolineal():
-    #obtener el json de la llamada post
-
-    #enviar el json a la funcion de la api linealx
-
-    #retornar el json de la respuesta
-    return {"mensaje": "apiNoLineal"};
+    #Aqui se procesan los datos
 
 
+    return data  # Devuelve la lista de listas
+
+@app.post("/apinolineal")
+async def nolineal(request: Request):
+    # Se obtiene el json del cuerpo de la petición POST
+    data = await request.json() # Devuelve una lista de listas
+
+    #Aqui se procesan los datos
+
+    
+    return data  # Devuelve la lista de listas
