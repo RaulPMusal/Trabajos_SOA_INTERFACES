b4f2cdd77d535dbd1e3e6bad7c0d872fc7945f88
MikelInesta <mikel_inesta@hotmail.com>
2023-12-28 15:00:01 +0100

    dependencias de python en el manual y .docx

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from fastapi import Request, FastAPI
 import json
+import pca
+import tsne
 
 app = FastAPI()
 
 @app.post("/apilineal")
 async def lineal(request: Request):
     # Se obtiene el json del cuerpo de la petición POST
-    data = await request.json() # Devuelve una lista de listas
+    json = await request.json() # Devuelve una lista de listas
 
     #Aqui se procesan los datos
-
-
-    return data 
+    jsonReducido = tsne(json)
+    
+    return jsonReducido 
 
 @app.post("/apinolineal")
 async def nolineal(request: Request):
     # Se obtiene el json del cuerpo de la petición POST
-    data = await request.json() # Devuelve una lista de listas
+    json = await request.json() # Devuelve una lista de listas
 
     #Aqui se procesan los datos
-
+    jsonReducido = pca(json)
     
-    return data 
+    return jsonReducido 
