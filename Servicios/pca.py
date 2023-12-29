4146a220c255234f18d5094da146e78ff9477665
RaulPMusal <152325054+RaulPMusal@users.noreply.github.com>
2023-12-29 19:13:43 +0100

    ARREGLADO

--- Servicios/pca.py
+++ Servicios/pca.py
@@ -3,9 +3,7 @@ import json
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler 
-from sklearn.decomposition import PCA  # to apply PCA
-from sklearn.manifold import TSNE
-
+from sklearn.decomposition import PCA
 
 
 """
@@ -22,8 +20,7 @@ def reduccion_dimension_PCA(matriz_datos):
     # Escalamos los datos para que puedan ser comparados
     X = StandardScaler().fit_transform(matriz_datos)
     # Crear objeto del algoritmo PCA        
-    # Al poner el parámetro n_componets = 'mle' se usará Minka’s MLE para predecir la dimensión de los datos de salida de forma automática.
-    pca = PCA(n_components = 'mle')
+    pca = PCA()
     # Ajustar los datos al modelo y reducir estos
     resultados_pca = pca.fit_transform(X)
     # Convertir los resultados de PCA a listas de Python
@@ -39,8 +36,7 @@ def reduccion_dimension_PCA(matriz_datos):
         'varianza': varianza,
     }
     
-
-    return json.dumps(resultado_final_auto)
+    return resultado_final_auto
 
 
 
@@ -53,22 +49,18 @@ def reduccion_dimension_PCA(matriz_datos):
     RETORNO
     * resultado json con los datos reducidos y/o mensaje para el usuario
 """
-def reduccionPSA(datos_json):
-    
+def reduccionPCA(datos_json):
+        
     # Cargar datos del JSON y convertir a un diccionario Python
     datos_dict = json.loads(datos_json)
 
-    # Convertir los datos del diccionario (strings) en floats (números)
-    #datos_dict['datos'] = [[float(num) for num in sublista] for sublista in datos_dict['datos']]
-    
-    # Convertir los datos a reducir a una matriz NumPy
+    # Convertir la lista de listas a una matriz NumPy
     datos_array = np.array(datos_dict['datos'])
-    
-    # Obtener el método de reducción de dimensión elegido por el usuario
-    #metodo_reduccion = datos_dict['metodo']
-      
+              
     # Ver el tipo de datos a reducir (True si son todos los datos numéricos | False si no)
     tipo_datos = np.all([np.issubdtype(type(x), np.number) for x in np.ravel(datos_array)])
+    
+    print(tipo_datos)
         
     # Si NO son todos los datos numéricos
     if tipo_datos != True:
@@ -76,7 +68,6 @@ def reduccionPSA(datos_json):
         # Crear un JSON de respusta y lo devolvemos
         resultado_final = {
             'datos': datos_dict['datos'], # datos originales
-            #'metodo': datos_dict['metodo'], # metodo erroneo dado
             'error_method': "DATOS NO SOPORTADOS" #indicacion del error
         }
         return resultado_final
